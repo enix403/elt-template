09c06d38fca21da73aa50d9103edc60466435f0a
enix403 <fog.code000@gmail.com>
2021-10-11 20:17:26 +0000

    Big Tables

--- units/renderer/src/app/features/temp/temp.tsx
+++ units/renderer/src/app/features/temp/temp.tsx
@@ -1,112 +1,164 @@
 import React from 'react';
 import { NavPageView } from "app/layout/views";
 import { Card, Icon, Button } from '@blueprintjs/core';
+import Flags from 'country-flag-icons/react/3x2';
+
+import './style.scss';
+
+const TableColumnHeader: React.FC<{}> = ({ children }) => {
+    return (
+        <th>
+            {children}
+            <span style={{ margin: '0 10px' }}>
+                <Icon icon="filter" style={{ color: '#dc3545' }} />
+                <Icon icon="sort" style={{ color: '#dc3545', marginLeft: 6 }} />
+            </span>
+        </th>
+    );
+};
 
 const MyTable = React.memo(() => {
     return (
         <div className="table-wrapper">
             <div className="table-header">
-                <span className="title">List of Customers</span>
+                <span className="title center-text-flow">
+                    <Icon icon='heatmap' size={20} style={{ color: '#ffc107' }} />
+                    <span className="icon-text">List of Customers</span>
+                </span>
                 <Button
-                    text="Export"
+                    text="Export To Excel"
                     rightIcon="export"
                     intent="success"
+                    outlined={true}
                 />
             </div>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>
-                            Name
-                            <span style={{ margin: '0 10px' }}>
-                                <Icon icon="filter" />
-                                <Icon style={{ marginLeft: 6 }} icon="sort" />
-                            </span>
-                        </th>
-                        <th>Country</th>
-                        <th>Agent</th>
-                        <th>Date</th>
-                        <th>Balance</th>
-                        <th>Status</th>
-                        <th>Activity</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>James Butt</td>
-                        <td>Algeria</td>
-                        <td>Ioni Bowcher</td>
-                        <td>09/13/2015</td>
-                        <td>$70,663.00</td>
-                        <td>Unqualified</td>
-                        <td>14%</td>
-                    </tr>
+            <div className="table-responsive">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <TableColumnHeader>Name</TableColumnHeader>
+                            <TableColumnHeader>Country</TableColumnHeader>
+                            <TableColumnHeader>Agent</TableColumnHeader>
+                            <TableColumnHeader>Date</TableColumnHeader>
+                            <TableColumnHeader>Balance</TableColumnHeader>
+                            <TableColumnHeader>Status</TableColumnHeader>
+                            <TableColumnHeader>Activity</TableColumnHeader>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>James Match</td>
+                            <td>
+                                <div className="no-idea">
+                                    <div className="flag-holder">
+                                        <Flags.US style={{height: 20}} />
+                                    </div>
+                                    United States
+                                </div>
+                            </td>
+                            <td>Ioni Bowcher</td>
+                            <td>09/13/2015</td>
+                            <td>$70,663.00</td>
+                            <td>Unqualified</td>
+                            <td>14%</td>
+                        </tr>
+                        <tr>
+                            <td>James Match</td>
+                            <td>
+                                <div className="no-idea">
+                                    <div className="flag-holder">
+                                        <Flags.AR style={{height: 20}} />
+                                    </div>
+                                    Argentina
+                                </div>
+                            </td>
+                            <td>Ioni Bowcher</td>
+                            <td>09/13/2015</td>
+                            <td>$70,663.00</td>
+                            <td>Unqualified</td>
+                            <td>14%</td>
+                        </tr>
 
-                    <tr>
-                        <td>James Butt</td>
-                        <td>Algeria</td>
-                        <td>Ioni Bowcher</td>
-                        <td>09/13/2015</td>
-                        <td>$70,663.00</td>
-                        <td>Unqualified</td>
-                        <td>14%</td>
-                    </tr>
-                    <tr>
-                        <td>James Butt</td>
-                        <td>Algeria</td>
-                        <td>Ioni Bowcher</td>
-                        <td>09/13/2015</td>
-                        <td>$70,663.00</td>
-                        <td>Unqualified</td>
-                        <td>14%</td>
-                    </tr>
-                    <tr>
-                        <td>James Butt</td>
-                        <td>Algeria</td>
-                        <td>Ioni Bowcher</td>
-                        <td>09/13/2015</td>
-                        <td>$70,663.00</td>
-                        <td>Unqualified</td>
-                        <td>14%</td>
-                    </tr>
-                    <tr>
-                        <td>James Butt</td>
-                        <td>Algeria</td>
-                        <td>Ioni Bowcher</td>
-                        <td>09/13/2015</td>
-                        <td>$70,663.00</td>
-                        <td>Unqualified</td>
-                        <td>14%</td>
-                    </tr>
-                    <tr>
-                        <td>James Butt</td>
-                        <td>Algeria</td>
-                        <td>Ioni Bowcher</td>
-                        <td>09/13/2015</td>
-                        <td>$70,663.00</td>
-                        <td>Unqualified</td>
-                        <td>14%</td>
-                    </tr>
-                    <tr>
-                        <td>James Butt</td>
-                        <td>Algeria</td>
-                        <td>Ioni Bowcher</td>
-                        <td>09/13/2015</td>
-                        <td>$70,663.00</td>
-                        <td>Unqualified</td>
-                        <td>14%</td>
-                    </tr>
-                    <tr>
-                        <td>James Butt</td>
-                        <td>Algeria</td>
-                        <td>Ioni Bowcher</td>
-                        <td>09/13/2015</td>
-                        <td>$70,663.00</td>
-                        <td>Unqualified</td>
-                        <td>14%</td>
-                    </tr>
-                </tbody>
-            </table>
+                        <tr>
+                            <td>James Match</td>
+                            <td>
+                                <div className="no-idea">
+                                    <div className="flag-holder">
+                                        <Flags.BS style={{height: 20}} />
+                                    </div>
+                                    Algeria
+                                </div>
+                            </td>
+                            <td>Ioni Bowcher</td>
+                            <td>09/13/2015</td>
+                            <td>$70,663.00</td>
+                            <td>Unqualified</td>
+                            <td>14%</td>
+                        </tr>
+
+                        <tr>
+
+                            <td>James Match</td>
+                            <td>
+                                <div className="no-idea">
+                                    <div className="flag-holder">
+                                        <Flags.AG style={{height: 20}} />
+                                    </div>
+                                    Antigua and Barbuda
+                                </div>
+                            </td>
+                            <td>Ioni Bowcher</td>
+                            <td>09/13/2015</td>
+                            <td>$70,663.00</td>
+                            <td>Unqualified</td>
+                            <td>14%</td>
+                        </tr>
+
+                        <tr>
+                            <td>James Match</td>
+                            <td>
+                                <div className="no-idea">
+                                    <div className="flag-holder">
+                                        <Flags.CA style={{height: 20}} />
+                                    </div>
+                                    Canada
+                                </div>
+                            </td>
+                            <td>Ioni Bowcher</td>
+                            <td>09/13/2015</td>
+                            <td>$70,663.00</td>
+                            <td>Unqualified</td>
+                            <td>14%</td>
+                        </tr>
+                        <tr>
+                            <TableColumnHeader>Name</TableColumnHeader>
+                            <TableColumnHeader>Country</TableColumnHeader>
+                            <TableColumnHeader>Agent</TableColumnHeader>
+                            <TableColumnHeader>Date</TableColumnHeader>
+                            <TableColumnHeader>Balance</TableColumnHeader>
+                            <TableColumnHeader>Status</TableColumnHeader>
+                            <TableColumnHeader>Activity</TableColumnHeader>
+                        </tr>
+                        <tr>
+                            <td>James Match</td>
+                            <td>
+                                <div className="no-idea">
+                                    <div className="flag-holder">
+                                        <Flags.SV style={{height: 20}} />
+                                    </div>
+                                    El Salvador
+                                </div>
+                            </td>
+                            <td>Ioni Bowcher</td>
+                            <td>09/13/2015</td>
+                            <td>$70,663.00</td>
+                            <td>Unqualified</td>
+                            <td>14%</td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 });
